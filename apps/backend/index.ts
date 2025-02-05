f4f0efda2304fc254d0919573774753ba7a11cf8
André Coelho <andrevrcoelho@hotmail.com>
2025-02-05 14:22:23 -0300

    docs: add swagger

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,9 +1,68 @@
 import Fastify from "fastify";
+import { Item } from "./types";
 
 const fastify = Fastify();
 
-fastify.get("/", function (_, reply) {
-  reply.send({ hello: "world" });
+let items: Item[] = [];
+
+fastify.register(import("@fastify/swagger"));
+
+fastify.register(import("@fastify/swagger-ui"), {
+  routePrefix: "/documentation",
+  uiConfig: {
+    docExpansion: "full",
+    deepLinking: false,
+  },
+  uiHooks: {
+    onRequest: function (request, reply, next) {
+      next();
+    },
+    preHandler: function (request, reply, next) {
+      next();
+    },
+  },
+  staticCSP: true,
+  transformStaticCSP: (header) => header,
+  transformSpecification: (swaggerObject, request, reply) => {
+    return swaggerObject;
+  },
+  transformSpecificationClone: true,
+});
+
+fastify.get(
+  "/",
+  {
+    schema: {
+      description: "Get items",
+      tags: ["item"],
+      summary: "qwerty",
+      response: {
+        201: {
+          description: "Successful response",
+          type: "object",
+          properties: {
+            id: { type: "number" },
+            name: { type: "string" },
+          },
+        },
+      },
+    },
+  },
+  (_, reply) => {
+    reply.send(items);
+  }
+);
+
+fastify.post("/", {}, function (reqest, reply) {
+  const item = reqest.body as Item;
+  items.push(item);
+  reply.send(item);
+});
+
+fastify.delete("/:itemId", function (reqest, reply) {
+  const params = reqest.params as any;
+  items = items.filter((i) => i.id !== Number(params.itemId));
+  reply.code(204).send();
 });
 
 fastify.listen({ port: 3030 }, function (err) {
